7dc3b6f928300913d4cdd7cf52f51a43623d4ac1
AndLydakis <andlydakis@gmail.com>
2017-02-21 18:31:41 -0500

    gdffgd

--- scripts/three_joint_publisher.py
+++ scripts/three_joint_publisher.py
@@ -162,9 +162,9 @@ class SinglePublisher:
                                 joints.get(frame).parent).pitch
 
                         t2.transform.rotation = t.transform.rotation
-                        joints.get(frame).x = joints.get(frame).radius * math.sin(joints.get(frame).pitch) * math.cos(
+                        joints.get(frame).x = joints.get(frame).radius * math.sin(joints.get(frame).pitch) * math.sin(
                             joints.get(frame).yaw)
-                        joints.get(frame).y = joints.get(frame).radius * math.sin(joints.get(frame).pitch) * math.sin(
+                        joints.get(frame).y = joints.get(frame).radius * math.sin(joints.get(frame).pitch) * math.cos(
                             joints.get(frame).yaw)
                         joints.get(frame).z = joints.get(frame).radius * math.cos(joints.get(frame).pitch)
 
