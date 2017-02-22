36971c8c1a9d68b3dcf5caaf72a26440b3b4babd
AndLydakis <andlydakis@gmail.com>
2017-02-22 14:37:13 -0500

    hgf

--- scripts/three_joint_publisher.py
+++ scripts/three_joint_publisher.py
@@ -4,11 +4,7 @@ import rospy
 import tf
 import tf2_ros
 import geometry_msgs.msg
-import serial
-import time
 import math
-import string
-import glob
 import sensor_table
 import find_dng
 import threespace as tsa
@@ -18,17 +14,9 @@ from socket import *
 from threespace_ros.msg import dataVec
 
 
-def quaternion_to_msg(q):
-    msg = Quaternion()
-    msg.x = q[0]
-    msg.y = q[1]
-    msg.z = q[2]
-    msg.w = q[3]
-    return msg
-
-
 class SinglePublisher:
     def __init__(self):
+        global id_
         rospy.init_node("publisher")
         r = rospy.Rate(100)
         suffix = "_data_vec"
@@ -80,7 +68,6 @@ class SinglePublisher:
 
         t = geometry_msgs.msg.TransformStamped()
         t2 = geometry_msgs.msg.TransformStamped()
-        t3 = geometry_msgs.msg.TransformStamped()
         dv = dataVec()
         dev_list = []
         for d in dongle_list:
@@ -116,17 +103,12 @@ class SinglePublisher:
                         dv.comY = full[7]
                         dv.comZ = full[8]
 
-                        t.header.stamp = rospy.Time.now()
-                        t2.header.stamp = rospy.Time.now()
-                        t3.header.stamp = rospy.Time.now()
+                        t.header.stamp = t2.header.stamp = t2.header.stamprospy.Time.now()
 
-                        t.header.frame_id = joints.get(frame).parent
-                        t2.header.frame_id = joints.get(frame).parent
-                        t3.header.frame_id = joints.get(frame).parent
+                        t.header.frame_id = t2.header.frame_id = joints.get(frame).parent
 
                         t.child_frame_id = joints.get(frame).name
                         t2.child_frame_id = joints.get(frame).name + '2'
-                        t3.child_frame_id = joints.get(frame).name + '3'
 
                         (roll, pitch, yaw) = tf.transformations.euler_from_quaternion(
                             [t.transform.rotation.x,
@@ -135,7 +117,6 @@ class SinglePublisher:
                              t.transform.rotation.w])
                         if yaw == 0.0 or pitch == 0.0 or roll == 0.0:
                             continue
-
                         if joints.get(frame).set is False:
                             joints.get(frame).initYaw = yaw
                             joints.get(frame).intPitch = pitch
@@ -148,20 +129,20 @@ class SinglePublisher:
                             joints.get(frame).roll = roll - joints.get(frame).initRoll
                         if abs(joints.get(frame).pitch - pitch - joints.get(frame).initPitch) > 0.01:
                             joints.get(frame).pitch = pitch - joints.get(frame).initPitch
-                            rospy.logerr([yaw, pitch, roll])
-                            rospy.logerr([joints.get(frame).yaw, joints.get(frame).pitch, joints.get(frame).roll])
-                            rospy.logerr([math.cos(joints.get(frame).yaw),
-                                                  math.cos(joints.get(frame).pitch),
-                                                  math.cos(joints.get(frame).roll)])
-                            rospy.logerr([math.sin(joints.get(frame).yaw),
-                                                    math.sin(joints.get(frame).pitch),
-                                                    math.sin(joints.get(frame).roll)])
-                            rospy.logerr([joints.get(frame).x, joints.get(frame).y, joints.get(frame).z])
-                            rospy.logerr("*******************************")
                         if abs(joints.get(frame).yaw - yaw - joints.get(frame).initYaw) > 0.01:
                             joints.get(frame).yaw = yaw - joints.get(frame).initYaw
 
-                        # q = tf.transformations.quaternion_from_euler(roll, pitch, yaw)
+                        # rospy.logerr([yaw, pitch, roll])
+                        # rospy.logerr([joints.get(frame).yaw, joints.get(frame).pitch, joints.get(frame).roll])
+                        # rospy.logerr([math.cos(joints.get(frame).yaw),
+                        #               math.cos(joints.get(frame).pitch),
+                        #               math.cos(joints.get(frame).roll)])
+                        # rospy.logerr([math.sin(joints.get(frame).yaw),
+                        #               math.sin(joints.get(frame).pitch),
+                        #               math.sin(joints.get(frame).roll)])
+                        # rospy.logerr([joints.get(frame).x, joints.get(frame).y, joints.get(frame).z])
+                        # rospy.logerr("*******************************")
+
                         q = tf.transformations.quaternion_from_euler(joints.get(
                             frame).roll, joints.get(frame).pitch, joints.get(frame).yaw)
                         msg = geometry_msgs.msg.Quaternion(q[0], q[1], q[2], q[3])
@@ -169,24 +150,14 @@ class SinglePublisher:
                         empty = geometry_msgs.msg.Quaternion(eq[0], eq[1], eq[2], eq[3])
                         t.transform.rotation = msg
                         t2.transform.rotation = empty
-                        t3.transform.rotation = t.transform.rotation
 
-                        # joints.get(frame).x = joints.get(frame).radius * math.sin(joints.get(frame).pitch) * math.sin(
-                        #     joints.get(frame).yaw)
-                        # joints.get(frame).y = joints.get(frame).radius * math.cos(joints.get(frame).pitch) * math.sin(
-                        #     joints.get(frame).yaw)
-                        # joints.get(frame).z = joints.get(frame).radius * math.cos(joints.get(frame).pitch)
-
-                        joints.get(frame).x = joints.get(frame).radius *\
-                                              math.cos(joints.get(frame).yaw)*\
+                        joints.get(frame).x = joints.get(frame).radius * math.cos(joints.get(frame).yaw) * \
                                               math.cos(joints.get(frame).pitch)
 
-                        joints.get(frame).y = joints.get(frame).radius *\
-                                              math.cos(joints.get(frame).pitch) *\
+                        joints.get(frame).y = joints.get(frame).radius * math.cos(joints.get(frame).pitch) * \
                                               math.sin(joints.get(frame).yaw)
 
-                        joints.get(frame).z = - joints.get(frame).radius *\
-                                              math.sin(joints.get(frame).pitch)
+                        joints.get(frame).z = - joints.get(frame).radius * math.sin(joints.get(frame).pitch)
 
                         t.transform.translation.x = joints.get(frame).x
                         t.transform.translation.y = joints.get(frame).y
@@ -195,33 +166,20 @@ class SinglePublisher:
                         t2.transform.translation.y = joints.get(frame).y * 2
                         t2.transform.translation.z = joints.get(frame).z * 2
 
-                        # t.transform.translation.x = joints.get(frame).radius * math.sin(
-                        #     joints.get(frame).pitch) * math.cos(joints.get(frame).yaw)
-                        # t.transform.translation.y = joints.get(frame).radius * math.sin(
-                        #     joints.get(frame).pitch) * math.sin(joints.get(frame).yaw)
-                        # t.transform.translation.z = joints.get(frame).radius * math.cos(joints.get(frame).yaw)
-                        # t2.transform.translation.x = joints.get(frame).radius * 2 * math.sin(
-                        #     joints.get(frame).pitch) * math.cos(joints.get(frame).yaw)
-                        # t2.transform.translation.y = joints.get(frame).radius * 2 * math.sin(
-                        #     joints.get(frame).pitch) * math.sin(joints.get(frame).yaw)
-                        # t2.transform.translation.z = joints.get(frame).radius * 2 * math.cos(joints.get(frame).yaw)
-
                         br = tf2_ros.TransformBroadcaster()
                         br2 = tf2_ros.TransformBroadcaster()
-                        #  br3 = tf2_ros.TransformBroadcaster()
 
                         br.sendTransform(t)
                         br2.sendTransform(t2)
-                        # br3.sendTransform(t3)
                         dp.publish(dv)
                     else:
                         # rospy.logerr("None")
                         pass
         for d in dongle_list:
             tsa.TSDongle.close(d)
-        print publishers
-        print dv_publishers
-        print broadcasters
+        print(publishers)
+        print(dv_publishers)
+        print(broadcasters)
         exit()
 
 
