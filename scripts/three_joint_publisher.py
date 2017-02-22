dbb2f3e8b5cbda3fc219b2f8497af470b9077b2f
AndLydakis <andlydakis@gmail.com>
2017-02-21 20:05:37 -0500

    gdffgd

--- scripts/three_joint_publisher.py
+++ scripts/three_joint_publisher.py
@@ -96,6 +96,7 @@ class SinglePublisher:
 
         t = geometry_msgs.msg.TransformStamped()
         t2 = geometry_msgs.msg.TransformStamped()
+        t3 = geometry_msgs.msg.TransformStamped()
         dv = dataVec()
         dev_list = []
         for d in dongle_list:
@@ -132,13 +133,23 @@ class SinglePublisher:
                         dv.comX = full[6]
                         dv.comY = full[7]
                         dv.comZ = full[8]
+
                         t.header.stamp = rospy.Time.now()
                         t2.header.stamp = rospy.Time.now()
+                        t3.header.stamp = rospy.Time.now()
+
                         t.header.frame_id = joints.get(frame).parent
                         t2.header.frame_id = joints.get(frame).parent
+                        t3.header.frame_id = joints.get(frame).parent
+
                         t.child_frame_id = joints.get(frame).name
                         t2.child_frame_id = joints.get(frame).name + '2'
+                        t3.child_frame_id = joints.get(frame).name + '3'
+
                         t.transform.rotation = dv.quat.quaternion
+                        t2.transform.rotation = dv.quat.quaternion
+                        t3.transform.rotation = dv.quat.quaternion
+
                         (yaw, pitch, roll) = tf.transformations.euler_from_quaternion(
                             [t.transform.rotation.x,
                              t.transform.rotation.y,
@@ -169,12 +180,16 @@ class SinglePublisher:
                             joints.get(frame).z = joints.get(frame).radius * math.cos(joints.get(frame).pitch)
 
                             # rospy.logerr(str(joints.get(frame).x)+" "+str(joints.get(frame).y)+" "+str(joints.get(frame).z))
-                            t.transform.translation.x = joints.get(frame).x
-                            t.transform.translation.y = joints.get(frame).y
-                            t.transform.translation.z = joints.get(frame).z
-                            t2.transform.translation.x = joints.get(frame).x * 2
-                            t2.transform.translation.y = joints.get(frame).y * 2
-                            t2.transform.translation.z = joints.get(frame).z * 2
+
+                        t.transform.translation.x = joints.get(frame).x
+                        t.transform.translation.y = joints.get(frame).y
+                        t.transform.translation.z = joints.get(frame).z
+                        t2.transform.translation.x = joints.get(frame).x * 2
+                        t2.transform.translation.y = joints.get(frame).y * 2
+                        t2.transform.translation.z = joints.get(frame).z * 2
+                        t3.transform.translation.x = joints.get(frame).x * 3
+                        t3.transform.translation.y = joints.get(frame).y * 3
+                        t3.transform.translation.z = joints.get(frame).z * 3
 
                         # t.transform.translation.x = joints.get(frame).radius * math.sin(
                         #     joints.get(frame).pitch) * math.cos(joints.get(frame).yaw)
@@ -188,10 +203,12 @@ class SinglePublisher:
                         # t2.transform.translation.z = joints.get(frame).radius * 2 * math.cos(joints.get(frame).yaw)
 
                         br = tf2_ros.TransformBroadcaster()
-                        # br2 = tf2_ros.TransformBroadcaster()
+                        br2 = tf2_ros.TransformBroadcaster()
+                        br3 = tf2_ros.TransformBroadcaster()
 
                         br.sendTransform(t)
-                        # br2.sendTransform(t2)
+                        br2.sendTransform(t2)
+                        br3.sendTransform(t3)
                         dp.publish(dv)
                     else:
                         # rospy.logerr("None")
