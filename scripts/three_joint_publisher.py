8d8e5ac1e79e388715d1317c1d2292a4043ac4f1
AndLydakis <andlydakis@gmail.com>
2017-02-22 13:29:32 -0500

    dsfgsdfg

--- scripts/three_joint_publisher.py
+++ scripts/three_joint_publisher.py
@@ -181,7 +181,7 @@ class SinglePublisher:
                                               math.cos(joints.get(frame).pitch)
 
                         joints.get(frame).y = joints.get(frame).radius *\
-                                              math.sin(joints.get(frame).pitch + 1.57079632679) *\
+                                              math.sin(joints.get(frame).pitch) *\
                                               math.cos(joints.get(frame).yaw)
 
                         joints.get(frame).z = joints.get(frame).radius * math.cos(joints.get(frame).pitch)
