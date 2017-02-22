fba408b2510c080594619d1e7ec39fa14b6366f2
AndLydakis <andlydakis@gmail.com>
2017-02-22 11:25:58 -0500

    dsfgsdfg

--- scripts/three_joint_publisher.py
+++ scripts/three_joint_publisher.py
@@ -161,21 +161,27 @@ class SinglePublisher:
                         t2.transform.rotation = t.transform.rotation
                         t3.transform.rotation = t.transform.rotation
 
-                        joints.get(frame).x = joints.get(frame).radius * math.sin(joints.get(frame).pitch) * math.sin(
-                            joints.get(frame).yaw)
+                        # joints.get(frame).x = joints.get(frame).radius * math.sin(joints.get(frame).pitch) * math.sin(
+                        #     joints.get(frame).yaw)
+                        # joints.get(frame).y = joints.get(frame).radius * math.cos(joints.get(frame).pitch) * math.sin(
+                        #     joints.get(frame).yaw)
+                        # joints.get(frame).z = joints.get(frame).radius * math.cos(joints.get(frame).pitch)
+
+                        joints.get(frame).x = joints.get(frame).radius * math.cos(joints.get(frame).pitch)
+
                         joints.get(frame).y = joints.get(frame).radius * math.cos(joints.get(frame).pitch) * math.sin(
                             joints.get(frame).yaw)
                         joints.get(frame).z = joints.get(frame).radius * math.cos(joints.get(frame).pitch)
 
                         t.transform.translation.x = joints.get(frame).x * 2
-                        t.transform.translation.y = joints.get(frame).y * 2
-                        t.transform.translation.z = joints.get(frame).z * 2
+                        t.transform.translation.y = 0
+                        t.transform.translation.z = 0
                         t2.transform.translation.x = joints.get(frame).x
-                        t2.transform.translation.y = joints.get(frame).y
-                        t2.transform.translation.z = joints.get(frame).z
+                        t2.transform.translation.y = 0
+                        t2.transform.translation.z = 0
                         t3.transform.translation.x = joints.get(frame).x * 3
-                        t3.transform.translation.y = joints.get(frame).y * 3
-                        t3.transform.translation.z = joints.get(frame).z * 3
+                        t3.transform.translation.y = 0
+                        t3.transform.translation.z = 0
 
                         # t.transform.translation.x = joints.get(frame).radius * math.sin(
                         #     joints.get(frame).pitch) * math.cos(joints.get(frame).yaw)
@@ -189,12 +195,12 @@ class SinglePublisher:
                         # t2.transform.translation.z = joints.get(frame).radius * 2 * math.cos(joints.get(frame).yaw)
 
                         br = tf2_ros.TransformBroadcaster()
-                        br2 = tf2_ros.TransformBroadcaster()
-                        br3 = tf2_ros.TransformBroadcaster()
+                        # br2 = tf2_ros.TransformBroadcaster()
+                        #  br3 = tf2_ros.TransformBroadcaster()
 
                         br.sendTransform(t)
-                        br2.sendTransform(t2)
-                        br3.sendTransform(t3)
+                        # br2.sendTransform(t2)
+                        # br3.sendTransform(t3)
                         dp.publish(dv)
                     else:
                         # rospy.logerr("None")
